5fd17afe19524d7222ec31e80c11dc3413bdf470
Denis <denis.basenko@yandex.ru>
2023-03-17 23:56:17 +0300

    created error page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom'
+
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <div className="text-4xl text-blue-600">404. Страница не найдена</div>
+      <div className="mt-4 text-gray-500">
+        Возможно, она была перемещена, или вы <br />
+        просто неверно указали адресс страницы
+      </div>
+      <Link to="/" className="mt-4 text-xl">
+        Перейти на главную
+      </Link>
+    </div>
+  )
 }
 
 export default Error
